fef98f338b504a743746f262c42c525fa221c17b
domingos <mds.junior87@gmail.com>
2024-10-05 11:20:54 -0300

    :lipstick: feat: Estilização a formatação para a letra central

---
+++ FlashFocus_003.py
@@ -0,0 +1,41 @@
+import time
+from tkinter import *
+import tkinter as tk
+
+def encontrar_letra_central(palavra):
+    """Encontra e retorna a letra central de uma palavra."""
+    tamanho = len(palavra)
+    if tamanho == 0:
+        return "" 
+    indice_meio = tamanho // 2 
+    return palavra[indice_meio]
+
+def exibir_palavras():
+    """Exibe as palavras do texto e suas letras centrais."""
+    texto = caixa_texto.get("1.0", tk.END).strip()
+    palavras = texto.split()
+
+    for palavra in palavras:
+        letra_central = encontrar_letra_central(palavra)
+        texto_exibicao = f"{palavra} {letra_central}" 
+        label_palavra.config(text=texto_exibicao)
+        janela.update()
+        time.sleep(0.3)
+
+# Cria a janela principal
+janela = tk.Tk()
+janela.title("Exibidor de Palavras")
+
+# Cria um label para exibir as palavras
+label_palavra = tk.Label(janela, text="", font=("Helvetica", 24))
+label_palavra.pack(pady=20)
+
+# Cria uma caixa de texto para inserir o texto
+caixa_texto = tk.Text(janela, height=5, width=30)
+caixa_texto.pack()
+
+# Cria um botão para iniciar a exibição
+botao_exibir = tk.Button(janela, text="Exibir Palavras", command=exibir_palavras)
+botao_exibir.pack(pady=10)
+
+janela.mainloop()
---
+++ FlashFocus_004.py
@@ -0,0 +1,54 @@
+import time
+from tkinter import *
+import tkinter as tk
+
+def encontrar_letra_central(palavra):
+    """Encontra e retorna a letra central de uma palavra."""
+    tamanho = len(palavra)
+    if tamanho == 0:
+        return "" 
+    indice_meio = tamanho // 2 
+    return palavra[indice_meio]
+
+def exibir_palavras():
+   
+    texto = caixa_texto.get("1.0", tk.END).strip()
+    palavras = texto.split()
+
+    for palavra in palavras:
+        letra_central = encontrar_letra_central(palavra)
+        indice_meio = len(palavra) // 2
+
+        parte1 = palavra[:indice_meio]
+        parte2 = palavra[indice_meio + 1:]
+        texto_exibicao = f"{parte1}{letra_central}{parte2}"
+
+
+        label_palavra.config(text=texto_exibicao)
+        # Configuração adicional para habilitar tags HTML no Label
+        label_palavra.config(wraplength=300) # Ajuste conforme necessário
+        label_palavra.config(justify='center')
+        janela.update()
+        time.sleep(0.3)
+
+
+
+
+# Cria a janela principal
+janela = tk.Tk()
+
+janela.title("Exibidor de Palavras")
+
+# Cria um label para exibir as palavras
+label_palavra = tk.Label(janela, text="", font=("Helvetica", 24))
+label_palavra.pack(pady=20)
+
+# Cria uma caixa de texto para inserir o texto
+caixa_texto = tk.Text(janela, height=5, width=30)
+caixa_texto.pack()
+
+# Cria um botão para iniciar a exibição
+botao_exibir = tk.Button(janela, text="Exibir Palavras", command=exibir_palavras)
+botao_exibir.pack(pady=10)
+
+janela.mainloop()
